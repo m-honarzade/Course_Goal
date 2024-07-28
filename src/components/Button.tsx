41e009e4af0eee98ddc176469a868be91b2092e9
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-07-28 11:23:07 +0330

    edit button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,12 @@
-const Button = ({ type, children }) => {
+import { ComponentPropsWithoutRef, ReactNode } from "react";
+
+type ButtonProps = {
+  children: ReactNode;
+} & ComponentPropsWithoutRef<"button">;
+const Button = ({ children, ...props }: ButtonProps) => {
   return (
     <button
-      type={type}
+      {...props}
       className=" w-full p-2 rounded-sm bg-[#f7e596] text-[#3a4346] text-sm font-extrabold cursor-pointer"
     >
       {children}
